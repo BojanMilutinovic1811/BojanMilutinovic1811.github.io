6103fa7cba8af14899c3b5b33aae3bba2b3b2461
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-10-31 10:50:29 +0100

    scrollReveal update

---
+++ js/scrollReveal.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal();
+
+
+sr.reveal('.section-content', {
+    duration: 2000,
+    easing: 'cubic-bezier(.72,-0.38,.39,1.42)',
+    // opacity: 0.4,
+    scale: 0.6
+});
+
+sr.reveal('#cv', {
+    duration: 2000,
+    easing: 'cubic-bezier(.72,-0.38,.39,1.42)',
+    scale: 0.4
+});
