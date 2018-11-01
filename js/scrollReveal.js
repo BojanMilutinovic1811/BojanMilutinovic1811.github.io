af1f84cfab36d897303952787b8d3c6d90627519
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2018-11-01 09:18:05 +0100

    github search project update

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -2,14 +2,12 @@ window.sr = ScrollReveal();
 
 
 sr.reveal('.section-content', {
-    duration: 2000,
-    easing: 'cubic-bezier(.72,-0.38,.39,1.42)',
-    // opacity: 0.4,
-    scale: 0.6
+    duration: 3000,
+    opacity: 0.3
 });
 
 sr.reveal('#cv', {
-    duration: 2000,
+    duration: 1500,
     easing: 'cubic-bezier(.72,-0.38,.39,1.42)',
     scale: 0.4
 });
