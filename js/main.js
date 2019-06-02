f5f6ebc993754b62066366c87ef2a5e5e6914e4c
BojanMilutinovic1811 <bojanmilutinovic1811@gmail.com>
2019-06-02 20:16:23 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,125 +1,131 @@
-(function(){
-    const menuBtn = document.getElementById('menu-btn'),
-    menu = document.querySelector('.menu'),
-    open = document.getElementById('open'),
-    letters = document.querySelectorAll('.letters'),
-    openList = document.querySelectorAll('.openList'),
-    quoteText = document.getElementById('quote-text'),
-    quoteAuthor = document.getElementById('quote-author'),
-    menuItems = document.querySelectorAll('.menu-item');
+(function() {
+  const menuBtn = document.getElementById("menu-btn"),
+    menu = document.querySelector(".menu"),
+    open = document.getElementById("open"),
+    letters = document.querySelectorAll(".letters"),
+    openList = document.querySelectorAll(".openList"),
+    quoteText = document.getElementById("quote-text"),
+    quoteAuthor = document.getElementById("quote-author"),
+    menuItems = document.querySelectorAll(".menu-item");
 
-let showMenu = false,
-  lightsInterval = 700,
-  dropDelay = lightsInterval * letters.length + 300;
+  let showMenu = false,
+    lightsInterval = 700,
+    dropDelay = lightsInterval * letters.length + 300;
 
-const quotes = [
-  {
-      quote: "Possession make you rich? I don't have that type of richness. My richness is Life, forever.",
-   author: "Bob Marley"
-  },
-  {
-      quote: "Do you think that I count the days? There is only one day left, always starting over. It is given to us at dawn and taken away from us at dusk.",
-   author: "Jean-Paul Sartre"
-  },
-   {
-       quote:"Alice: How long is forever? White Rabbit: Sometimes, just one second.",
-    author:"Lewis Carroll"
-  },
-   {
-       quote:"There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.",
-    author:"Albert Einstein"
-  },
-   {
-       quote:"Some of us think holding on makes us strong but sometimes it is letting go.",
-    author:"Herman Hesse"
-  },
-   {
-       quote:"And those who were seen dancing were thought to be insane by those who could not hear the music.",
-    author:"Friedrich Nietzsche"
-  },
-   {
-       quote:"I don’t care that they stole my idea. I care that they don’t have any of their own.",
-    author:"Nikola Tesla"
-  },
-   {
-       quote:"Talent hits a target no one else can hit; Genius hits a target no one else can see.",
-    author:"Arthur Schopenhauer"
-  },
-   {
-       quote:"I'm not in this world to live up to your expetations and you're not in this world to live up to mine.",
-    author:"Bruce Lee"
-  },
-   {
-       quote:"Right or wrong, it's very pleasant to break something from time to time.",
-    author:"Fyodor Dostoevsky"
-  },
-   {
-       quote:"There are people we laugh at because they can’t see the most obvious things about themselves. Well, those people are us!",
-    author:"Ezra Bayda"
-  },
-];
+  const quotes = [
+    {
+      quote:
+        "Possession make you rich? I don't have that type of richness. My richness is Life, forever.",
+      author: "Bob Marley"
+    },
+    {
+      quote:
+        "Do you think that I count the days? There is only one day left, always starting over. It is given to us at dawn and taken away from us at dusk.",
+      author: "Jean-Paul Sartre"
+    },
+    {
+      quote:
+        "Alice: How long is forever? White Rabbit: Sometimes, just one second.",
+      author: "Lewis Carroll"
+    },
+    {
+      quote:
+        "There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.",
+      author: "Albert Einstein"
+    },
+    {
+      quote:
+        "Some of us think holding on makes us strong but sometimes it is letting go.",
+      author: "Herman Hesse"
+    },
+    {
+      quote:
+        "And those who were seen dancing were thought to be insane by those who could not hear the music.",
+      author: "Friedrich Nietzsche"
+    },
+    {
+      quote:
+        "I don’t care that they stole my idea. I care that they don’t have any of their own.",
+      author: "Nikola Tesla"
+    },
+    {
+      quote:
+        "Talent hits a target no one else can hit; Genius hits a target no one else can see.",
+      author: "Arthur Schopenhauer"
+    },
+    {
+      quote:
+        "I'm not in this world to live up to your expetations and you're not in this world to live up to mine.",
+      author: "Bruce Lee"
+    },
+    {
+      quote:
+        "Right or wrong, it's very pleasant to break something from time to time.",
+      author: "Fyodor Dostoevsky"
+    },
+    {
+      quote:
+        "There are people we laugh at because they can’t see the most obvious things about themselves. Well, those people are us!",
+      author: "Ezra Bayda"
+    }
+  ];
 
+  // transform menu button on click and show the menu
 
-// transform menu button on click and show the menu
+  menuBtn.addEventListener("click", transformBtn);
 
-menuBtn.addEventListener('click', transformBtn);
+  //menu items close menu on click
 
-//menu items close menu on click
+  menuItems.forEach(item => {
+    item.addEventListener("click", closeMenu);
+  });
 
-menuItems.forEach(item => {
-  item.addEventListener('click', closeMenu)
-});
+  // drop open element on main page
 
-// drop open element on main page 
+  document.addEventListener("DOMContentLoaded", dropOpen);
 
-document.addEventListener('DOMContentLoaded', dropOpen);
+  //functions
 
-
-//functions 
-
-function transformBtn() {
-
-  if(!showMenu) {
-      menuBtn.classList.add('btn-close');
-      menu.classList.add('menu-show');
-      let quoteNum = Math.floor(Math.random()*quotes.length);
+  function transformBtn() {
+    if (!showMenu) {
+      menuBtn.classList.add("btn-close");
+      menu.classList.add("menu-show");
+      let quoteNum = Math.floor(Math.random() * quotes.length);
       quoteText.textContent = quotes[quoteNum].quote;
       quoteAuthor.textContent = quotes[quoteNum].author;
       showMenu = true;
-  } else {
-     closeMenu();
+    } else {
+      closeMenu();
+    }
   }
-}
 
-function closeMenu() {
-  menuBtn.classList.remove('btn-close');
-  menu.classList.remove('menu-show');
-  showMenu = false;
-}
+  function closeMenu() {
+    menuBtn.classList.remove("btn-close");
+    menu.classList.remove("menu-show");
+    showMenu = false;
+  }
 
-function dropOpen() {
-  counterClass();
-  setTimeout(()=> {
-      openList.forEach(li => li.classList.add('drop-li'))
-      open.classList.add('drop');
-  }, dropDelay)
-}
+  function dropOpen() {
+    counterClass();
+    setTimeout(() => {
+      openList.forEach(li => li.classList.add("drop-li"));
+      open.classList.add("drop");
+    }, dropDelay);
+  }
 
-function counterClass() {
-  let counter = 0; 
-  let interval = setInterval(()=> {
-      if(counter<letters.length) {
-          letters[counter].classList.add('text-shadow');
-          counter++;
+  function counterClass() {
+    let counter = 0;
+    let interval = setInterval(() => {
+      if (counter < letters.length) {
+        letters[counter].classList.add("text-shadow");
+        counter++;
       } else {
-          clearInterval(interval)
+        clearInterval(interval);
       }
-  }, lightsInterval)
-}
+    }, lightsInterval);
+  }
 
+  // particlesJS.load('particles-js', 'particles.json', function() {
 
-
-particlesJS.load('particles-js', 'particles.json', function() {
-    
-});
+  // });
 })();
--- js/scrollReveal.js
+++
@@ -1,7 +0,0 @@
-window.sr = new ScrollReveal({ mobile: false })
-
-sr.reveal('#cv', {
-    duration: 1500,
-    easing: 'cubic-bezier(.72,-0.38,.39,1.42)',
-    scale: 0.4
-});
